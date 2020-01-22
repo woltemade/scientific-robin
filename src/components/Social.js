3a269dc8f8d430596a379da02c523c2854bf04cc
woltemade <herald.smit@gmail.com>
2020-01-22 18:58:38 +0200

    testpreview

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import _ from 'lodash';
+import React from "react";
+import _ from "lodash";
 
-import {Link} from '../utils';
+import { Link } from "../utils";
 
 export default class Social extends React.Component {
-    render() {
-        return (
-            <ul className="social-links">
-              {_.map(_.get(this.props, 'pageContext.site.data.social.links'), (link, link_idx) => (
-              <li key={link_idx}>
-                <Link to={_.get(link, 'url')} target="_blank" rel="noopener">{_.get(link, 'title')}</Link>
-              </li>
-              ))}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <ul className="social-links">
+        {_.map(
+          _.get(this.props, "pageContext.site.data.social.links"),
+          (link, link_idx) => (
+            <li key={link_idx}>
+              <Link to={_.get(link, "url")} target="_blank" rel="noopener">
+                {_.get(link, "title")} test
+              </Link>
+            </li>
+          )
+        )}
+      </ul>
+    );
+  }
 }
